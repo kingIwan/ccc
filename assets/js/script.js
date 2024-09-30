5c4f9b7b6205bab5db4c7627f723234a5fe8b394
Lokesh Bansal <lokesh@75way.com>
2024-09-30 14:25:37 +0530

    worked on ip address text change

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+function loadLanguage(langCode) {
+    let langFile = '/assets/languages/lang-en.json'; 
+
+    if (langCode === 'BR') {
+        langFile = '/assets/languages/lang-pt.json'; 
+    } else if (langCode === 'ES') {
+        langFile = '/assets/languages/lang-es.json'; 
+    }
+    fetch(langFile)
+        .then(response => response.json())
+        .then(data => {
+            applyTranslations(data); 
+        })
+        .catch(error => {
+            console.error("Error loading language file:", error);
+        });
+}
+
+function applyTranslations(translations) {
+    const elements = document.querySelectorAll('[data-i18n]');
+
+    elements.forEach(element => {
+        const key = element.getAttribute('data-i18n');
+
+        if (translations[key]) {
+            element.innerText = translations[key];
+        }
+    });
+}
+function getCountryCode() {
+    fetch('http://www.geoplugin.net/json.gp')
+        .then(response => response.json())
+        .then(data => {
+            const countryCode = data.geoplugin_countryCode;
+            loadLanguage(countryCode);
+        })
+        .catch(error => {
+            console.error("Error fetching geolocation data:", error);
+            loadLanguage('EN'); 
+        });
+}
+
+getCountryCode();
